b29c2033401ee6dcbc9499800688e61cfc987c31
Sahil Bacchus <sahilbacchus@gmail.com>
2025-04-11 20:36:24 -0600

    fixed a bug, where using the navbar would give an error

--- my-lms-app/src/components/Header.js
+++ my-lms-app/src/components/Header.js
@@ -6,12 +6,6 @@ import logoImage from "./logo.jpg";
 function Header() {
     return (
       <div className="AppHeader">
-        <head>
-            <meta charset="UTF-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <title>LMS Homepage</title>
-            <link rel="stylesheet" type="text/css" href="styles.css"/>
-        </head>
         <header>
             <img src={logoImage} alt="LMS Logo" style={{width: '100px', height: '100px'}}/>
             <h1>LMS - Learning Management System</h1>
